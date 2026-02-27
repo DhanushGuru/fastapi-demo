b73dd61a9633f12a5d21d462389e9668001aa584
DhanushGuru <126089208+DhanushGuru@users.noreply.github.com>
2026-02-27 13:58:08 +0000

    Solved CORS

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # For development only
-    # allow_credentials=True,
+    allow_credentials=True,
     allow_methods=["*"],
-    # allow_headers=["*"],
+    allow_headers=["*"],
 )
 
 database_models.Base.metadata.create_all(bind=engine)
