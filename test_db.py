15ee33918d8ffa824bde2dd46cfa9a8fbfde584c
Dhanushguru <dguru2003@gmail.com>
2026-04-19 03:42:32 +0530

    Final commit with Sqlite

--- test_db.py
+++
@@ -1,9 +0,0 @@
-from database import engine
-
-try:
-    with engine.connect() as connection:
-        print(" DB connected successfull")
-except Exception as e:
-    print("Not connected",e)
-    
-
